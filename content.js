fa6b12c53ec03cfe20b0b36b2174625220a4eeb6
David Balash <dgbalash@gmail.com>
2020-07-30 11:21:05 -0400

    Added string cleaning for reviews. Added a next page click.

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 
-let reviewsData = '[';
+let reviewsData = '';
 
 document.querySelectorAll('.ba-pa').forEach(function(node) {
         let date = node.getElementsByClassName("ba-Eb-Nf")[0].innerText;
@@ -9,13 +9,18 @@ document.querySelectorAll('.ba-pa').forEach(function(node) {
         node.querySelectorAll('.rsw-stars').forEach( function (element) {
            stars = element.getAttribute('aria-label');
         });
-        let review = node.getElementsByClassName("ba-Eb-ba")[0].innerText;
+        
+        let review = node.getElementsByClassName("ba-Eb-ba")[0].innerText
+            .replace('"', '')
+            .replace("'", "")
+            .replace('\n', ' ');
+
         reviewsData = reviewsData + '\n{"date": "' + date
             + '", "name": "' + name
             + '", "stars": "' + stars
             + '", "review": "' + review +'"},';
 });
 
-reviewsData = reviewsData.substring(0, reviewsData.length - 1) + ']';
-
 browser.runtime.sendMessage({type: "set_reviews", reviews: reviewsData});
+
+document.querySelectorAll('.Aa.dc-se').forEach(function(node) {node.click();});
--- popup.js
+++ popup.js
@@ -1,6 +1,4 @@
 
-browser.tabs.executeScript({file: "content.js"});
-
 browser.runtime.sendMessage({type: "get_reviews"}, reviews => {
     document.getElementById("reviews").value = reviews;
 });
